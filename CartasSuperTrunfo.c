636efc491291b9659fac93e74174d542e07b7311
thiagooscs <thiago_castro29@hotmail.com>
2025-04-16 17:30:37 +0000

    Iniciando os testes

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
     int main(){
         printf("desafio Super Trunfo!\n");
+       
+        //variavéis das cartas
+
+        char Estado1, Estado2;
+        char Código1 [5], Códido2 [5];
+        char Cidade1 [20], Cidade2 [20];
+        int População1 [50], População2[50];
+        float Area1, Area2;
+        float Pib1, Pib2;
+        int Pontos Turísticos1, Pontos Turísticos2;
     
 
 
