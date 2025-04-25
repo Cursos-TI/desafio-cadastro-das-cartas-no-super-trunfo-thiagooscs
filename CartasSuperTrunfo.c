4503c41c77bf03452537b6a05bfc76cedf3b68f3
thiagooscs <thiago_castro29@hotmail.com>
2025-04-25 18:52:33 +0000

    nível aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -33,6 +33,7 @@ int main() {
      
     printf("A Densidade Populacional de São Paulo é: %.2f hab/km²\n",DensidadePopulacionalSP);
     printf("O PIB per capita de São Paulo %.2fR$\n",PIBpercapitaSP);
+    printf("\n ");
   
   
     // Informações da carta 2
@@ -44,6 +45,16 @@ int main() {
     printf("PIB: %.2Lf R$\n", PIBrj);
     printf("Número de pontos de turísticos: %d\n", PontosTuristicosRJ);
     printf("\n");
+
+    //Densidade Populacional do Rio de Janeiro
+
+    float Densidade PopulacionalRJ = PopulacaoRJ / AreaRJ; // 5622.16 hab/km²
+    float PIBpercapitaRJ = PIBrj / PopulacaoRJ; //44457.62 R$
+
+    printf ("A Densidade Populacional do Rio de Janeiro é: %.2f hab/km²\n", DensidadePopulacionalRJ);
+    printf ("O PIB per capita do Rio de Janeiro é: .2fR$\n, PIBpercapitaRJ");
+    printf ("\n");
+
 
     return 0;
  }
